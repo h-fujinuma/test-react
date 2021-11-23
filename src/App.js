5fb1bf7e881a9c1ac556576ec6aca28854d5e5ce
h-fujinuma <fuji.2277339.sc@gmail.com>
2021-11-23 12:11:54 +0900

    make another component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import CallApi from './components/CallApi'
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <CallApi />
     </div>
   );
 }
---
+++ src/components/CallApi.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+//import axios from 'axios'
+
+const CallApi = () => {
+    return (
+        <div>
+            CallApi Component!!
+        </div>
+    )
+}
+
+export default CallApi
